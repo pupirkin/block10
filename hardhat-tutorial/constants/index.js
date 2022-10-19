67010fb34ac315e0d6a0dd6a0122b2468778d793
pupirkin <pupirkin02@gmail.com>
2022-10-19 13:49:56 +0600

    block10

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x60FDe2540b66c23acffF1FE5C22db6DAaC35DFC6";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
